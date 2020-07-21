49f4ef7f932c1ce49147729112a21d345270e425
Steven Tew <steve@steventew.co.uk>
2020-07-21 10:07:30 +0100

    Add lastName, phoneNumber, stylist, service and notes fields

--- appointments/src/AppointmentsDayView.js
+++ appointments/src/AppointmentsDayView.js
@@ -1,19 +1,73 @@
 import React, { useState } from 'react';
+import { render } from 'react-dom';
 
 const appointmentTimeOfDay = startsAt => {
   const [h, m] = new Date(startsAt).toTimeString().split(':');
   return `${h}:${m}`;
 }
 
-export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+export const Appointment = ({
+  customer,
+  service,
+  stylist,
+  notes,
+  startsAt
+}) => {
+  return (
+    <div id="appointmentView">
+      <h3>
+        Today's appointment at {appointmentTimeOfDay(startsAt)}
+      </h3>
+      <table>
+        <tbody>
+          <tr>
+            <td>Customer</td>
+            <td>
+              {customer.firstName} {customer.lastName}
+            </td>
+          </tr>
+          <tr>
+            <td>Phone number</td>
+            <td>
+              {customer.phoneNumber}
+            </td>
+          </tr>
+          <tr>
+            <td>Stylist</td>
+            <td>
+              {stylist}
+            </td>
+          </tr>
+          <tr>
+            <td>Service</td>
+            <td>
+              {service}
+            </td>
+          </tr>
+          <tr>
+            <td>Phone number</td>
+            <td>
+              {notes}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+};
+
 export const AppointmentsDayView = ({ appointments }) => {
   const [selectedAppointment, setSelectedAppointment] = useState(0);
   return (
     <div id="appointmentsDayView">
       <ol>
-        {appointments.map((appointment,i) => (
+        {appointments.map((appointment, i) => (
           <li key={appointment.startsAt}>
-            <button type="button" onClick={() => setSelectedAppointment(i)}>
+            <button
+              className={i === selectedAppointment ? 'toggled' : ''}
+              type="button"
+              onClick={() => setSelectedAppointment(i)}
+            >
               {appointmentTimeOfDay(appointment.startsAt)}
             </button>
           </li>

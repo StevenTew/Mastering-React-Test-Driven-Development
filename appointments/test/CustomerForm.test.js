562d37f3ea6575072c6adf32dfe26b079441a4ce
Steven Tew <steve@steventew.co.uk>
2020-07-23 07:12:43 +0100

    Add first name text field

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -5,14 +5,22 @@ import { CustomerForm } from "../src/CustomerForm";
 describe('CustomerForm', () => {
   let render, container;
 
+  const form = (id) => container.querySelector(`form[id="${id}"]`);
+
   beforeEach(() => {
     ({ render, container } = createContainer())
   })
 
   it('renders a form', () => {
     render(<CustomerForm />);
-    expect(
-      container.querySelector('form[id="customer"]')
-    ).not.toBeNull();
+    expect(form('customer')).not.toBeNull();
+  })
+
+  it('renders the first name field as a text box', () => {
+    render(<CustomerForm />);
+    const field = form('customer').elements.firstName;
+    expect(field).not.toBeNull();
+    expect(field.tagName).toEqual('INPUT');
+    expect(field.type).toEqual('text');
   })
 });
